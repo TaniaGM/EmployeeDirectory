d74e4f5ab7adbf64d0409a752668969c66d906b9
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-05-11 16:18:00 -0500

    re-organizing file structure and mapping

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import Container from "./components/Container"
+import React { Component } from 'react';
+import EmployeeCard from "./components/EmployeeCard/EmployeeCard";
+import Wrapper from "./components/Wrapper/";
+import Title from "./components/Title";
+import employees from "./employee.json"
 
+class App extends Component {
+  // Setting this.state.employees to the employees json array
+  state = {
+    employees
+  };
 
-function App() {
-  return (
-    <Container />
+  removeEmployee = id => {
+    // Filter this.state.employees for employees with an id not equal to the id being removed
+    const employees = this.state.employees.filter(employee => employee.id !== id);
+    // Set this.state.employees equal to the new emloyees array
+    this.setState({ employees });
+  };
 
-  )
+  // Map over this.state.employees and render a EmployeeCard component for each employee object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Employee Directory</Title>
+        {this.state.employees.map(employee => (
+          <EmployeeCard
+            removeEmployee={this.removeEmployee}
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            image={employee.image}
+            occupation={employee.occupation}
+            location={employee.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Search/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.css";
+
+const Search = ({ userinput, handler }) => {
+    return (
+        <nav className='searchbar'>
+            <label><h3>Search Employees:</h3></label>
+            <input name="userinput" onChange={handler} value={userinput} />
+        </nav>
+    );
+}
+
+export default Search;
